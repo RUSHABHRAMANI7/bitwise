3d359da80744724fe8cfda9da19abddfe573091c
RUSHABH <rushabhramani16@gmail.com>
2023-01-05 14:08:50 +0530

    add programm

---
+++ bitwise.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ; 
+
+int bitwise(int n ){
+int res = 0 ; 
+while (n>0){
+    res++; 
+
+    n=n>>1;
+}
+return res; 
+
+}
+int main(){
+    int n  ;
+    cout << "Enter the number "<< endl ;
+    cin >> n; 
+   cout <<  bitwise(n); 
+
+
+}
